01147fa5f3b6aceb06929ca005d5bb432077713c
Srinivas V <srinivasv147@gmail.com>
2017-05-14 18:09:11 +0530

    added the google login feature

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ var session = require("express-session");
 
 //configuration files
 var configDB = require("./config/configDB");
-require("./config/configPassport")(passport);
+require("./config/configPassport.js")(passport);
 // var index = require('./routes/index');
 // var users = require('./routes/users');
 
@@ -22,8 +22,8 @@ mongoose.connect(configDB.url);
 var app = express();
 
 // view engine setup
-// app.set('views', path.join(__dirname, 'views'));
-// app.set('view engine', 'jade');
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
@@ -34,10 +34,13 @@ app.use(cookieParser());
 app.use(session({ secret: 'SrinivasV147' }));
 app.use(passport.initialize());
 app.use(passport.session());
-app.use(express.static(path.join(__dirname, 'public')));
+// app.use(express.static(path.join(__dirname, 'public')));
 
 // app.use('/', index);
 // app.use('/users', users);
+
+//use the routes created in the routes directory
+require("./routes/routes.js")(app,passport);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -46,20 +49,22 @@ app.use(function(req, res, next) {
   next(err);
 });
 
+
+
 // error handler
 app.use(function(err, req, res, next) {
   // set locals, only providing error in development
   // res.locals.message = err.message;
   // res.locals.error = req.app.get('env') === 'development' ? err : {};
-
+  console.log(err);
   // render the error page
   res.status(err.status || 500);
-  res.end("there has been a server error. We will solve it soon");
-  // res.render('error');
+  // res.end("there has been a server error. We will solve it soon");
+  res.render('error');
 
 });
 
-//use the routes created in the routes directory
-require("./routes/routes.js")(app,passport);
+
+
 
 module.exports = app;
--- routes/routes.js
+++ routes/routes.js
@@ -1,3 +1,50 @@
 module.exports = function(app,passport){
-  
+  console.log("function executed");
+
+  app.get('/',function(req,res){
+    console.log("got request");
+    res.render('../public/index.ejs');
+  });
+
+  app.get('/login',function(req,res){
+    res.render('../public/login.ejs')
+  });
+
+  // app.post('/login',function(req,res){
+  //   //here we get login info.
+  // });
+
+  app.get('/profile',isLoggedIn,function(req,res){
+    res.render('../public/profile.ejs')
+  });
+
+  app.get('/auth/google',passport.authenticate('google',{'scope':['profile','email']}));
+
+  app.get('/auth/google/callback',passport.authenticate(
+    'google',{
+      successRedirect : '/profile',
+      failureRedirect : '/'
+    }
+  ));
+
+  app.get('/logout',isLoggedIn,function(req,res){
+    req.logout();
+    res.render('/');
+  });
+  // console.log(app);
+};
+
+function isLoggedIn (req,res,next){
+  console.log(req.isAuthenticated.toString());
+  if(req.isAuthenticated()){
+    return next();
+  }
+  else{
+    res.redirect('/');
+  }
 }
+
+// function isRun(req,res,err){
+//   console.log("callback is run");
+//   return next();
+// }
